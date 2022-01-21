0b7cf3fa5ec78d554e5e3d85c901b209bbb64dfe
lannie520 <lj2433@columbia.edu>
2022-01-21 12:27:28 +0800

    upload files

---
+++ caesar.py
@@ -0,0 +1,25 @@
+import math
+def encrypt(key,plaintext):
+    ciphertext=""
+    #YOUR CODE HERE
+    list = []
+    for character in plaintext:
+        a = chr(ord(character) + key)
+        list.append(a)
+    ciphertext = list
+    return ciphertext
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    #YOUR CODE HERE
+    list = []
+    for character in ciphertext:
+        b = chr(ord(character)- key)
+        list.append(b)
+    plaintext = list
+    return plaintext
+
+if __name__ == '__main__':
+    print(encrypt(1, "WELL"))
+    print(decrypt(1, "DONE"))
+
